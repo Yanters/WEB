87f20b12f8cce75e61b265c5c8af3727e3a9a535
43695512 <yanter@wp.pl>
2021-06-13 21:31:12 +0200

    Todo List JS

--- Testing-Javascript/testing-js.js
+++ Testing-Javascript/testing-js.js
@@ -12,37 +12,89 @@
 // } else {
 //     console.log(`Bad password: ${password}`)
 // }
-let numberOfTries = 0;
-let lastguess = '';
-let guess = '';
-let hint = '';
-let guessednumbers = "";
-let lose = true;
-let winningnumber = Math.floor(Math.random() * 1000);
-console.log(winningnumber);
-while (lose) {
-    if (guess != null && guess != '') {
-        if (guess > winningnumber) {
-            hint = 'Lower';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numberOfTries = 0;
+// let lastguess = '';
+// let guess = '';
+// let hint = '';
+// let guessednumbers = "";
+// let lose = true;
+// let winningnumber = Math.floor(Math.random() * 1000);
+// console.log(winningnumber);
+// while (lose) {
+//     if (guess != null && guess != '') {
+//         if (guess > winningnumber) {
+//             hint = 'Lower';
+//         } else {
+//             hint = 'Higher';
+//         }
+//     }
+//     guess = prompt(`Guess your number:\nHint: ${hint} \nUsed numbers: ` + guessednumbers);
+//     numberOfTries++;
+//     if (guess != null && guess != '') {
+//         if (!guessednumbers.includes(' ' + guess + ' ') && lastguess != guess) {
+//             guessednumbers += " " + guess;
+//         }
+//         if (guess == winningnumber) {
+//             lose = false;
+//             console.log(`You won! \nTry number: ${numberOfTries}`)
+//             alert(`You won! \nTry number: ${numberOfTries}`);
+//             break;
+
+//         }
+//     }
+
+//     lastguess = guess;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let input = prompt('what would you like to do?');
+const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('*****************')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*****************')
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Ok, enter an index to delete:'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
         } else {
-            hint = 'Higher';
+            console.log('Unknown index')
         }
     }
-    guess = prompt(`Guess your number:\nHint: ${hint} \nUsed numbers: ` + guessednumbers);
-    numberOfTries++;
-    if (guess != null && guess != '') {
-        if (!guessednumbers.includes(' ' + guess + ' ') && lastguess != guess) {
-            guessednumbers += " " + guess;
-        }
-        if (guess == winningnumber) {
-            lose = false;
-            console.log(`You won! \nTry number: ${numberOfTries}`)
-            alert(`You won! \nTry number: ${numberOfTries}`);
-            break;
-
-        }
-    }
-
-    lastguess = guess;
+    input = prompt('what would you like to do?')
 }
-
+console.log('OK QUIT THE APP!')
