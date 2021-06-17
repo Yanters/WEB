f0bf307b37a56a62ae87775a300a528f91ba8a35
43695512 <yanter@wp.pl>
2021-06-18 00:02:46 +0200

    Form Events

---
+++ Form-Events/form-js.js
@@ -0,0 +1,19 @@
+const tweetForm = document.querySelector('#tweetForm');
+const tweetContainer = document.querySelector('#tweets');
+
+tweetForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let usernameInput = tweetForm.elements.username;
+    let tweetInput = tweetForm.elements.tweet;
+    addTweet(usernameInput.value, tweetInput.value);
+    usernameInput.value = ''; tweetInput.value = '';
+})
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username);
+    newTweet.append(bTag);
+    newTweet.append(` - ${tweet}`)
+    tweetContainer.append(newTweet);
+}
