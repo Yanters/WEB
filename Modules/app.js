488cc93641dfd3db1839cb307753d7e31e0923f2
43695512 <yanter@wp.pl>
2021-06-24 22:23:17 +0200

    Modules

---
+++ Modules/app.js
@@ -0,0 +1,4 @@
+const math = require('./math');
+
+const number = math.PI;
+console.log(number);
---
+++ Modules/math.js
@@ -0,0 +1,8 @@
+const add = (x, y) => x + y;
+const square = x => x * x;
+
+const PI = 3.14159;
+
+module.exports.add = add;
+module.exports.square = square;
+module.exports.PI = PI;
