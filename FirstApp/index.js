2bb54d2f55226577711df3bd4532e04dbdd4beab
43695512 <yanter@wp.pl>
2021-06-25 23:33:07 +0200

    First-Live-App

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -16,18 +16,32 @@ app.get('/r/:sublink', (req, res) => {
     const { sublink } = req.params;
     res.send(`<center><h1>Some directory: "/r/${sublink}"</h1></center>`);
 })
+app.get('/r/:sublink/:userID', (req, res) => {
+    const { sublink, userID } = req.params;
+    res.send(`<center><h1>Some directory: "/r/${sublink}"</br>User ID: ${userID}</h1></center>`);
+})
 
 app.get('/cats', (req, res) => {
-    res.send('<h1>/cats <==</h1>');
+    res.send('<center><h1>/cats <==</h1></center>');
 })
 
 app.post('/cats', (req, res) => {
-    res.send('Post request to "/cats"');
+    res.send('<center><h1>Post request to "/cats"</center></h1>');
 })
 
 app.get('/dogs', (req, res) => {
-    res.send('<h1>/dogs <==</h1>');
+    res.send('<center><h1>/dogs <==</h1></center>');
 })
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    if (!q) {
+        res.send(`<h1><center>Are you searching something?</h1></center>`)
+    }
+    res.send(`<h1><center>Search results to: ${q}</h1></center>`)
+    console.log(req.query);
+})
+
 app.get('*', (req, res) => {
     res.send('<center><h1>Page not found</br>Error #404</center></h1>');
 })
