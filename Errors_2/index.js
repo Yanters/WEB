2cbd6ddf803c8b105a6989eb0d2b75031147a3db
43695512 <yanter@wp.pl>
2021-07-05 22:09:44 +0200

    Errors_2

--- Errors_2/index.js
+++ Errors_2/index.js
@@ -4,7 +4,10 @@ const path = require('path');
 const app = express();
 const methodOverride = require('method-override');
 
+const AppError = require('./AppError');
+
 const Product = require('./models/product');
+const { stat } = require('fs');
 
 mongoose.connect('mongodb://localhost:27017/farmStand2', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -37,38 +40,70 @@ app.get('/products', async (req, res) => {
 })
 
 app.get('/products/new', (req, res) => {
+    // throw new AppError('Testing', 500);
     res.render('products/new', { categories });
 })
 
-app.post('/products', async (req, res) => {
-    // console.log(req.body);
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-    res.redirect(`/products/${newProduct._id}`)
+app.post('/products', async (req, res, next) => {
+    try {
+        // console.log(req.body);
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+        res.redirect(`/products/${newProduct._id}`)
+    }
+    catch (e) {
+        next(e)
+    }
 })
 
-app.get('/products/:id', async (req, res) => {
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e));
+    }
+}
+
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
+
     const { id } = req.params;
     const product = await Product.findById(id);
     // console.log(product);
+    if (!product) {
+        throw new AppError('Product not found', 500);
+    }
     res.render('products/show', { product });
-})
+
+}))
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
     res.redirect('/products');
 })
-app.get('/products/:id/edit', async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.render('products/edit', { product, categories })
+app.get('/products/:id/edit', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.render('products/edit', { product, categories })
+    } catch (e) {
+        next(e)
+    }
 })
 
-app.put('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
-    console.log(req.body);
-    res.redirect(`/products/${product._id}`)
+app.put('/products/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+        console.log(req.body);
+        res.redirect(`/products/${product._id}`)
+    }
+    catch (e) {
+        next(e)
+    }
+})
+
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Error' } = err;
+    res.status(status).send(message);
 })
 
 app.listen(3000, () => {
--- Errors_2/seeds.js
+++ Errors_2/seeds.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Product = require('./models/product');
 
-mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/farmStand2', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to DB.');
     })
