3b08aa436818a1b9732055de94e698a65d9aa165
43695512 <yanter@wp.pl>
2021-08-12 22:15:49 +0200

    dynamic data

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  const expenseDate = new Date(23);
+  const expenseTitle = "Car insurance";
+  var expenseAmount = (Math.random() * 500).toFixed(2);
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
