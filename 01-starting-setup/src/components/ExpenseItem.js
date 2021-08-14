343c7fa1469129361f880c31598c445f9a342fb1
43695512 <yanter@wp.pl>
2021-08-14 20:39:57 +0200

    New Card Composition

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.expenses.date} />
       <div className="expense-item__description">
         <h2>{props.expenses.title}</h2>
         <div className="expense-item__price">${props.expenses.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,12 +1,13 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 function Expenses(props) {
   const expenseList = props.expenses.map((item) => (
     <ExpenseItem expenses={item} />
   ));
 
-  return <div className="expenses">{expenseList}</div>;
+  return <Card className="expenses">{expenseList}</Card>;
 }
 
 export default Expenses;
