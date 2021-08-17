2949a1fc056a488416ee9458cd374abda1c6c861
43695512 <yanter@wp.pl>
2021-08-17 22:37:03 +0200

    Child-to-Parent Component Communication

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.expenses.title);
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expenses.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.expenses.title}</h2>
         <div className="expense-item__price">${props.expenses.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,13 +1,33 @@
+import React, { useState } from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [year, getSelectedYear] = useState("2021");
+
   const expenseList = props.expenses.map((item) => (
     <ExpenseItem expenses={item} key={item.id.toString()} />
   ));
 
-  return <Card className="expenses">{expenseList}</Card>;
+  const getSelectedYearHandler = (year) => {
+    getSelectedYear(year);
+    console.log(year);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={year}
+          getSelectedYear={getSelectedYearHandler}
+        />
+        {expenseList}
+      </Card>
+    </div>
+  );
 };
 
 export default Expenses;
