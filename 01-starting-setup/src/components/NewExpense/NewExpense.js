6524821c89c8c383376013c57836b44912b92f9f
43695512 <yanter@wp.pl>
2021-08-18 20:42:55 +0200

    Showing and Hiding form

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,18 +1,37 @@
+import React, { useState } from "react";
+
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.floor(Math.random() * 100).toString(),
     };
     props.onAddExpense(expenseData);
+    setShowForm(false);
+  };
+
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+  const hideFormHandler = () => {
+    setShowForm(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {showForm ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={hideFormHandler}
+        />
+      ) : (
+        <button onClick={showFormHandler}>Add new Expense</button>
+      )}
     </div>
   );
 };
