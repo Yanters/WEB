53b89b8482bed094b83e616fb3f1e32886595a40
43695512 <yanter@wp.pl>
2021-08-18 19:56:35 +0200

    New rendering data

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,15 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.expenses.date} />
-      <div className="expense-item__description">
-        <h2>{props.expenses.title}</h2>
-        <div className="expense-item__price">${props.expenses.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.expenses.date} />
+        <div className="expense-item__description">
+          <h2>{props.expenses.title}</h2>
+          <div className="expense-item__price">${props.expenses.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
