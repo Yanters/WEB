813907c2ebaaec8a4b40e859d9991deb71193928
43695512 <yanter@wp.pl>
2021-08-22 20:37:14 +0200

    Input Form and Button UI

---
+++ React/UsersList/src/components/Users/AddUser.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username:</label>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+        ></input>
+        <label htmlFor="age">Age:</label>
+        <input
+          id="age"
+          type="number"
+          step="1"
+          min="1"
+          max="120"
+          onChange={ageChangeHandler}
+        ></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
