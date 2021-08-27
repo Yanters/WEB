79762338fc3476668f83e7f576ad8e0a5ec9a8f7
43695512 <yanter@wp.pl>
2021-08-27 19:38:57 +0200

    Context Provider Component

--- React/LoginPanel/src/components/Login/Login.js
+++ React/LoginPanel/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "UserInput") {
@@ -24,7 +25,7 @@ const passwordReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
-const Login = (props) => {
+const Login = () => {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState("");
@@ -68,6 +69,8 @@ const Login = (props) => {
     };
   }, [emailIsValid, passwordIsValid]);
 
+  const ctx = useContext(AuthContext);
+
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "UserInput", val: event.target.value });
   };
@@ -88,7 +91,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- React/LoginPanel/src/store/auth-context.js
+++ React/LoginPanel/src/store/auth-context.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const LogInHangler = () => {
+    localStorage.setItem("IsLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  const LogOutHangler = () => {
+    localStorage.removeItem("IsLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const storedUserLoggedInData = localStorage.getItem("IsLoggedIn");
+
+    if (storedUserLoggedInData === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: LogOutHangler,
+        onLogin: LogInHangler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
